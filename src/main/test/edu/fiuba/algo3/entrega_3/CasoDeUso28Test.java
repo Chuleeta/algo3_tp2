036da2449836a0ef688a3f48e22d6abe97d85748
lebarrionuevo <lautarobarrionuevo0@gmail.com>
2022-11-24 11:52:16 -0300

    Fin del primer test del cu28 y comienzo del segundo

--- src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso28Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso28Test.java
@@ -15,17 +15,16 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CasoDeUso28Test {
-
     @Test
-    public void testUnZealotSeVuelveInvisible() throws RequerimientosInsuficientesException, NoExisteEdificioCorrelativoException {
+    public void testUnZealotSeVuelveInvisibleAlMatarTresUnidadesEnemigas() throws RequerimientosInsuficientesException, NoExisteEdificioCorrelativoException {
 
         Mineral mineral = new Mineral(1200);
         GasVespeno gas = new GasVespeno(300);
         Mapa mapa = new Mapa();
-        Zealot zealot = new Zealot(mineral, new Posicion(1, 2), mapa);
-        Zerling zerlingUno = new Zerling(mineral, new Posicion(1,1), mapa);
-        Zerling zerlingDos = new Zerling(mineral, new Posicion(2,1), mapa);
-        Zerling zerlingTres = new Zerling(mineral, new Posicion(3,1), mapa);
+        Zealot zealot = new Zealot(mineral, new Posicion(2, 2), mapa);
+        Zerling zerlingUno = new Zerling(mineral, new Posicion(2,3), mapa);
+        Zerling zerlingDos = new Zerling(mineral, new Posicion(1,2), mapa);
+        Zerling zerlingTres = new Zerling(mineral, new Posicion(3,2), mapa);
         AmoSupremo amoSupremo = new AmoSupremo(mineral, gas, new Posicion(1, 3), mapa);
 
         for (int i = 0; i < 7; i ++){
@@ -34,11 +33,52 @@ public class CasoDeUso28Test {
 
         assertFalse(zealot.invisible);
 
-        zealot.atacar(zerlingUno);
-        zealot.atacar(zerlingDos);
-        zealot.atacar(zerlingTres);
+        for (int i = 0; i < 5; i++){
+            zealot.atacar(zerlingUno);
+        }
+        for (int i = 0; i < 5; i++){
+            zealot.atacar(zerlingDos);
+        }
+        for (int i = 0; i < 5; i++){
+            zealot.atacar(zerlingTres);
+        }
 
         assertTrue(zealot.invisible);
 
     }
+
+    @Test
+    public void testUnZealotSeVuelveInvisibleAlMatarUnaCombinacionDeTresEnemigos() throws RequerimientosInsuficientesException, NoExisteEdificioCorrelativoException {
+
+        Mineral mineral = new Mineral(1200);
+        GasVespeno gas = new GasVespeno(300);
+        Mapa mapa = new Mapa();
+        //Criadero criadero = new Criadero(new Posicion(3,2), mapa);
+        Zealot zealot = new Zealot(mineral, new Posicion(2, 2), mapa);
+        Zerling zerlingUno = new Zerling(mineral, new Posicion(2,3), mapa);
+        Zerling zerlingDos = new Zerling(mineral, new Posicion(1,2), mapa);
+        Criadero criadero = new Criadero(new Posicion(3,2), mapa);
+        AmoSupremo amoSupremo = new AmoSupremo(mineral, gas, new Posicion(1, 3), mapa);
+
+        for (int i = 0; i < 7; i ++){
+            mapa.pasarTiempo();
+        }
+
+        assertFalse(zealot.invisible);
+
+        for (int i = 0; i < 5; i++){
+            zealot.atacar(zerlingUno);
+        }
+        for (int i = 0; i < 5; i++){
+            zealot.atacar(zerlingDos);
+        }
+        for (int i = 0; i < 63; i++){
+            zealot.atacar(criadero);
+        }
+
+        assertTrue(zealot.invisible);
+
+    }
+
+
 }
