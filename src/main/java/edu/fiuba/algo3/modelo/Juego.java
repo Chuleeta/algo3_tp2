191616c8edaf2fdf1c58908c01e8cca3619cb848
LUCAS <chulyfra40@gmail.com>
2022-12-05 18:56:30 -0300

    Arreglo el error de build

--- src/main/java/edu/fiuba/algo3/modelo/Juego.java
+++ src/main/java/edu/fiuba/algo3/modelo/Juego.java
@@ -4,14 +4,14 @@ import edu.fiuba.algo3.modelo.Exceptions.NoExisteEdificioCorrelativoException;
 import edu.fiuba.algo3.modelo.Exceptions.RequerimientosInsuficientesException;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.canvas.Canvas;
+//import javafx.scene.canvas.Canvas;
 
 public class Juego {
     private Mapa mapa;
     private Jugador jugadorUno;
     private Jugador jugadorDos;
 
-    private Canvas canvas;
+    //private Canvas canvas;
 
     public Juego(Mapa mapa, Jugador jugadorUno, Jugador jugadorDos) throws RequerimientosInsuficientesException{
         if(mapa == null || jugadorUno == null || jugadorDos == null) throw new RequerimientosInsuficientesException();
@@ -20,14 +20,14 @@ public class Juego {
         this.jugadorDos = jugadorDos;
     }
 
-    public Juego(Mapa mapa, Jugador jugadorUno, Jugador jugadorDos, Canvas canvasDado) throws RequerimientosInsuficientesException{
-        if(mapa == null || jugadorUno == null || jugadorDos == null) throw new RequerimientosInsuficientesException();
-        this.mapa = mapa;
-        this.jugadorUno = jugadorUno;
-        this.jugadorDos = jugadorDos;
+    // public Juego(Mapa mapa, Jugador jugadorUno, Jugador jugadorDos, Canvas canvasDado) throws RequerimientosInsuficientesException{
+    //     if(mapa == null || jugadorUno == null || jugadorDos == null) throw new RequerimientosInsuficientesException();
+    //     this.mapa = mapa;
+    //     this.jugadorUno = jugadorUno;
+    //     this.jugadorDos = jugadorDos;
 
-        this.canvas = canvasDado;
-    }
+    //     this.canvas = canvasDado;
+    // }
 
     public boolean pasarTiempo() throws NoExisteEdificioCorrelativoException {
         this.jugadorUno.pasarTiempo();
